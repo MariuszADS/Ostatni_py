a9655ff4bb4119af175998f023e946fe6e955298
Kacper <informatyka402@gmail.com>
2023-06-05 18:56:35 +0200

    python 05.06.2023

---
+++ 1.py
@@ -0,0 +1,53 @@
+import os
+os.getcwd()
+
+
+# print("""
+# instrukcja uruchamiania pliku przykład.py
+#
+#         --file [nazwa pliku]
+#             zapisuje output do pliku
+#
+#         --quiet
+#             wycisza logi w konsoli
+# Koniec.""")
+
+# --------------------
+# text = "i love python."
+# print(text)
+# print(text * 3)
+
+
+# --------------------
+
+
+# age = 27
+# name = "Paweł"
+#
+# print(name , str(age))
+# print(name + " " + str(age))
+# print(name , age)
+#
+#
+# print('{} ma {} lat'.format(name,age))
+# print(f"{age} i {name}")
+
+# -------------------------
+
+
+# saldo = 40
+# saldo += 10
+# saldo -= 22
+# print(saldo)
+# -------------------------
+
+
+# pixel = 150
+# pixel = pixel / 255
+# pixel /= 255
+# print(pixel)
+
+
+base = 2
+base **= 5
+print(base)
