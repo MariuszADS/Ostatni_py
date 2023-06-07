527180e82162d7070fc9ef8734afa61ccce74f9a
Kacper <informatyka402@gmail.com>
2023-06-07 23:12:02 +0200

    zabawa z tuplami

---
+++ tupla.py
@@ -0,0 +1,20 @@
+
+# TO UPORZADKOWANA STRUKTURA KTOREJ NIE MOZNA ZMIENIAC, NIE MOZNA AKTUALIZOWAC TUPLI
+
+empty_tuple = tuple()
+# print(tuple)
+
+amazon = ('amazon', 'usa', 'technology',1)
+google = ('google','usa','technology',2)
+# print(int(amazon[3]))
+
+
+# TUPLA ZAGNIEZDZONA
+
+data = (amazon,google)
+# print(data)
+
+# a = ('kacper','bak')
+imie,nazwisko,id_int = ('kacper','bak',int(12))
+data_2 = imie,nazwisko,id_int
+print(data_2)
